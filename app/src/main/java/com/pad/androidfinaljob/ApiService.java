48913674ec53d5abf76ea57c6bdf8c83cdcaf495
dxxhj <dxxhjk@126.com>
2020-06-05 10:46:26 +0800

    TODO: progress bar

--- app/src/main/java/com/pad/androidfinaljob/ApiService.java
+++ app/src/main/java/com/pad/androidfinaljob/ApiService.java
@@ -1,12 +1,8 @@
 package com.pad.androidfinaljob;
 
 import java.util.List;
-
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 
 public interface ApiService {
     // https://beiyou.bytedance.com/api/invoke/video/invoke/video
